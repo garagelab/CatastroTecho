ea0c5acdfec7e2c327a0a12f5d8aa3870395b3be
andreas <andreas.hempfling@gmail.com>
2013-10-15 07:39:56 -0300

    Cambio parámetros de sendgrid para heroku.

--- app.rb
+++ app.rb
@@ -134,23 +134,23 @@ class Techo < Sinatra::Base
 
  	post '/content/contact' do 
  		# SendGrid add-on in heroku for contact mails created at 15-oct-2013.
-# 		Pony.mail(
-# 			:from => params[:first_name] + " " + params[:last_name] + "<" + params[:email] + ">",
-# 		  	:to => 'andreas.hempfling@gmail.com', # cis.argentina@techo.org
-# 		  	:subject => "Nuevo mensaje de 'Relevamiento Argentina' - " + params[:first_name] + " " + params[:last_name],
-# 		  	:body => params[:message],
-# 		  	:port => '587',
-# 		  	:via => :smtp,
-# 		  	:via_options => { 
-# 				:address => 'smtp.sendgrid.net', 
-# 				:port => '587', 
-#     			:domain => 'heroku.com',
-#     			:user_name => ENV['app7793791@heroku.com'],
-#     			:password => ENV['kfgxawch'],
-#     			:authentication => :plain,
-#     			:enable_starttls_auto => true
-# 		  	}
-# 		)
+		Pony.mail(
+			:from => params[:first_name] + " " + params[:last_name] + " <" + params[:email] + ">",
+		  	:to => 'andreas.hempfling@gmail.com', # cis.argentina@techo.org
+		  	:subject => "Nuevo mensaje de 'Relevamiento Argentina' - " + params[:first_name] + " " + params[:last_name],
+		  	:body => "Nombre: " + params[:first_name] + " " + params[:last_name] + "\n" + "Email: " + params[:email] + "\n\n" "Mensaje: " + "\n" + params[:message],
+		  	:port => '587',
+		  	:via => :smtp,
+		  	:via_options => { 
+				:address => 'smtp.sendgrid.net', 
+				:port => '587', 
+    			:domain => 'heroku.com',
+    			:user_name => ENV['SENDGRID_USERNAME'],
+    			:password => ENV['SENDGRID_PASSWORD'],
+    			:authentication => :plain,
+    			:enable_starttls_auto => true
+		  	}
+		)
     	redirect '/content/success' 
 	end
 	
